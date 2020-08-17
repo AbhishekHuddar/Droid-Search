cd6444cdb2c95ea3c0a4033d9334304b6b2542fb
AbhishekHuddar <abhishekhuddar@gmail.com>
2020-08-17 16:48:37 -0500

    My first commit

---
+++ build/precache-manifest.41e75f57345bbae0b9204df38dd78e9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b915084d184b7e34395d67fb975cfa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cf2da9e01fd4137532d0",
+    "url": "/static/css/main.92bbfec4.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2da9e01fd4137532d0",
+    "url": "/static/js/main.c6f1ebf8.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
