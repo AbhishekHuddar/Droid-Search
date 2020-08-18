cd4fbd82368bd9f39fca3d55016be092849a4d68
AbhishekHuddar <abhishekhuddar@gmail.com>
2020-08-17 19:27:23 -0500

    Updates

---
+++ precache-manifest.39921a7864f738da7df8ac4d67f74144.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d043c55cbab18c8935ca7d2cdb57939a",
+    "url": "/Droid-Search/index.html"
+  },
+  {
+    "revision": "66a7124dbdf04d3fc253",
+    "url": "/Droid-Search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "081965af19f908d76519",
+    "url": "/Droid-Search/static/css/main.de0a93a3.chunk.css"
+  },
+  {
+    "revision": "66a7124dbdf04d3fc253",
+    "url": "/Droid-Search/static/js/2.592794b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Droid-Search/static/js/2.592794b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081965af19f908d76519",
+    "url": "/Droid-Search/static/js/main.54568038.chunk.js"
+  },
+  {
+    "revision": "a15885c1277359c368c6",
+    "url": "/Droid-Search/static/js/runtime-main.e6fe3587.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Droid-Search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
